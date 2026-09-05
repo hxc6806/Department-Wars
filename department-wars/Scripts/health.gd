484a8cd9288793c081fc5f160c8aca11c5c1c701
psf12 <ballanitariq@gmail.com>
2026-09-05 15:35:51 +1200

    Finished health script

--- department-wars/Scripts/health.gd
+++ department-wars/Scripts/health.gd
@@ -1,11 +1,37 @@
+class_name Health
 extends Node
 
+signal health_changed
+signal damaged
+signal healed
+signal died
 
-# Called when the node enters the scene tree for the first time.
+@export var max_health: int = 100
+var current_health: int
+
 func _ready() -> void:
-	pass # Replace with function body.
+	current_health = max_health
 
+func change_hp(amount) -> void:
+	if amount < 0: 
+		amount = max(0, current_health+amount)
+	else: 
+		amount = min(max_health, current_health+amount)
+		
+	current_health = amount
+	health_changed.emit()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func dmg(amount: int) -> void:
+	if isdead(): return
+	change_hp(-amount)
+	damaged.emit()
+	if current_health <= 0:
+		died.emit()
+		
+func heal(amount: int) -> void:
+	if isdead(): return
+	change_hp(amount)
+	healed.emit()
+	
+func isdead() -> bool:
+	return current_health <= 0
