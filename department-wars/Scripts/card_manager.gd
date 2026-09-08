09743890677c443c9255410b21e7a5ee5b3f091c
GibHut-3957 <cpv3957@autuni.ac.nz>
2026-09-09 00:19:00 +1200

    Created PlayerHand, CardSlot and Card Hover Effect

--- department-wars/Scripts/card_manager.gd
+++ department-wars/Scripts/card_manager.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 
 var screen_size: Vector2
 var card_being_dragged = null
+var is_hovering_on_card
+var player_hand_ref
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	player_hand_ref = $PlayerHand	
 
 func _process(_delta: float) -> void:
 	if card_being_dragged:
@@ -18,12 +22,10 @@ func _input(event):
 		if event.is_pressed():
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card
-				card_being_dragged.z_index = 10
+				start_drag(card)
 		else:
 			if card_being_dragged:
-				card_being_dragged.z_index = 0
-				card_being_dragged = null
+				finish_drag()
 
 func raycast_check_for_card():
 	var space_state = get_viewport().world_2d.direct_space_state
@@ -33,5 +35,71 @@ func raycast_check_for_card():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
+		return get_card_with_highest_z_index(result)
+	return null
+	
+func raycast_check_for_card_slot():
+	var space_state = get_viewport().world_2d.direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_viewport().get_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
 		return result[0].collider.get_parent()
 	return null
+
+func connect_card_signals(card):
+	card.connect("hovered", on_hovered_over_card)
+	card.connect("hovered_off", on_hovered_off_card)
+
+func on_hovered_over_card(card):
+	if not is_hovering_on_card and not card_being_dragged:
+		is_hovering_on_card = true
+		highlight_card(card, true)
+
+func on_hovered_off_card(card):
+	if not card_being_dragged:
+		var new_card_hovered = raycast_check_for_card()
+		if new_card_hovered and new_card_hovered != card:
+			highlight_card(new_card_hovered, true)
+			is_hovering_on_card = true
+		else:
+			is_hovering_on_card = false
+	highlight_card(card, false)
+
+func highlight_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1.0, 1.0)
+		card.z_index = 0
+
+func get_card_with_highest_z_index(cards):
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	for i in range(1, cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+			
+	return highest_z_card
+
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(1.0, 1.0)
+
+func finish_drag():
+	if card_being_dragged:
+		card_being_dragged.scale = Vector2(1.05, 1.05)
+		var card_slot_found = raycast_check_for_card_slot()
+		if card_slot_found and not card_slot_found.card_slotted:
+			card_being_dragged.position = card_slot_found.position
+			card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+			card_slot_found.card_slotted = true
+		else:
+			player_hand_ref.add_card_to_hand(card_being_dragged)
+	card_being_dragged = null
